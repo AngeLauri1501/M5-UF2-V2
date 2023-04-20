9143efe9a54d2232f0544c054f20dc0c0fe193cd
AngeLauri1501 <anla223@vidalibarraquer.net>
2023-04-20 09:26:35 +0200

    first commit

---
+++ src/main/java/com/mycompany/calcularareas/CalcularAreas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calcularareas;
+
+/**
+ *
+ * @author alumne
+ */
+public class CalcularAreas {
+
+    public static void main(String[] args) {
+       
+    }
+    public static int quadrat(int costat){
+        return costat*costat;
+    }
+}
---
+++ src/test/java/QuadratTest.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import com.mycompany.calcularareas.CalcularAreas;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author alumne
+ */
+public class QuadratTest {
+    
+    public QuadratTest() {
+    }
+    
+    @Test
+    public void QuadratAreaTest(){
+        assertEquals(36,CalcularAreas.quadrat(6));
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+}
